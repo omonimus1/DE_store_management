dafaf9035648f62bff03f861bd64a4ccd412f444
Davide Pollicino <davidepollicino2015@gmail.com>
2021-10-06 18:42:27 +0100

    creates basic project setup

---
+++ DE/DE/EDLoginForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.passwordLbl = new System.Windows.Forms.Label();
+            this.emailLbl = new System.Windows.Forms.Label();
+            this.pswtxtBox = new System.Windows.Forms.TextBox();
+            this.emailTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(414, 193);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 0;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(265, 193);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            // 
+            // passwordLbl
+            // 
+            this.passwordLbl.AutoSize = true;
+            this.passwordLbl.Location = new System.Drawing.Point(420, 85);
+            this.passwordLbl.Name = "passwordLbl";
+            this.passwordLbl.Size = new System.Drawing.Size(69, 17);
+            this.passwordLbl.TabIndex = 2;
+            this.passwordLbl.Text = "Password";
+            this.passwordLbl.Click += new System.EventHandler(this.passwordLabel_Click);
+            // 
+            // emailLbl
+            // 
+            this.emailLbl.AutoSize = true;
+            this.emailLbl.Location = new System.Drawing.Point(289, 85);
+            this.emailLbl.Name = "emailLbl";
+            this.emailLbl.Size = new System.Drawing.Size(42, 17);
+            this.emailLbl.TabIndex = 3;
+            this.emailLbl.Text = "Email";
+            this.emailLbl.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pswtxtBox
+            // 
+            this.pswtxtBox.Location = new System.Drawing.Point(398, 127);
+            this.pswtxtBox.Name = "pswtxtBox";
+            this.pswtxtBox.Size = new System.Drawing.Size(100, 22);
+            this.pswtxtBox.TabIndex = 4;
+            this.pswtxtBox.TextChanged += new System.EventHandler(this.pswtxtBox_TextChanged);
+            // 
+            // emailTxtBox
+            // 
+            this.emailTxtBox.Location = new System.Drawing.Point(256, 127);
+            this.emailTxtBox.Name = "emailTxtBox";
+            this.emailTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.emailTxtBox.TabIndex = 5;
+            this.emailTxtBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 478);
+            this.Controls.Add(this.emailTxtBox);
+            this.Controls.Add(this.pswtxtBox);
+            this.Controls.Add(this.emailLbl);
+            this.Controls.Add(this.passwordLbl);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ED management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label passwordLbl;
+        private System.Windows.Forms.Label emailLbl;
+        private System.Windows.Forms.TextBox pswtxtBox;
+        private System.Windows.Forms.TextBox emailTxtBox;
+    }
+}
+
