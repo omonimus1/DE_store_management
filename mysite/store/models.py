482c91c1d21a1ea1b12d85f237f8e608b88ab3a6
Davide Pollicino <davidepollicino2015@gmail.com>
2021-10-06 19:30:58 +0100

    Adds user model

--- mysite/store/models.py
+++ mysite/store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    level = models.IntegerField()
+
+    def __str__(self):
+        return self.name
