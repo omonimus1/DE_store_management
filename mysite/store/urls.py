ee4765d7a0a3f8d726faa3f351f52560d6d4ffc8
Davide Pollicino <davidepollicino2015@gmail.com>
2021-10-08 16:35:14 +0100

    adds basic ui project

---
+++ mysite/store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name="teacher"),
+    path('login', views.login),
+    path('product', views.product),
+    path('card', views.card),
+    path('offer', views.offer),
+    path('finance', views.finance)
+]
---
+++ mysite/store/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def product(request):
+    return render(request, 'product.html')
+
+def card(request):
+    return render(request, 'layality_card.html')
+
+def offer(request):
+    return render(request, 'offers.html')
+
+def finance(request):
+    return render(request, 'finance.html')
+
+    
