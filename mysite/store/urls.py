4063535eaf4ef5d2e3f4feff1a9a769cb37988f1
Davide Pollicino <davidepollicino2015@gmail.com>
2021-10-08 01:40:38 +0100

    #2 Adds login page

--- mysite/store/urls.py
+++ mysite/store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index,),
+    path('', views.index),
+    path('login', views.login),
 ]
--- mysite/store/views.py
+++ mysite/store/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
