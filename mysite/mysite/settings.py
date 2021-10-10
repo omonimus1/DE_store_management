fff44c50b0cbdd884bb8aad7af5ea0b6352f6b7e
Davide Pollicino <davidepollicino2015@gmail.com>
2021-10-10 18:42:47 +0100

    #14: Adds smtp configuration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Bottom of the file
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'davidepollicino2015@gmail.com'
+EMAIL_HOST_PASSWORD = 'CapitanAmerica4!'
