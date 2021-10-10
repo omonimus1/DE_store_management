8ca1b200a222ab54b3b4cfcdb3293d0b2e46e40a
Davide Pollicino <davidepollicino2015@gmail.com>
2021-10-10 03:51:35 +0100

    #6: fix user name if user is loggedin

--- mysite/store/serializers.py
+++ mysite/store/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Product, User, ProductImage
+from .models import Product, User, ProductImage,LoyalCard
 
 class ProductSerializer(serializers.HyperlinkModelSerializer):
 
