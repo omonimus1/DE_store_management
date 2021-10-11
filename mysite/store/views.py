1ff99b9f2cf89c4bb6690df1cc19d7bf5a758025
Davide Pollicino <davidepollicino2015@gmail.com>
2021-10-11 23:20:55 +0100

    Redirect to login if user is not logged in

--- mysite/store/views.py
+++ mysite/store/views.py
@@ -1,19 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .models import LoyalCard, Product, User, Offer, Category
-from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
+def is_user_authenticated(request):
+    user = request.user
+    if user.is_authenticated:
+        return True
+    return False
+
 def index(request):
-    return render(request, 'index.html')
+    if is_user_authenticated(request):
+        return render(request, 'index.html')
+    return loginPage(request)
+
 
 def create_user(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
+    if is_user_authenticated(request):
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+    return loginPage(request)
+
 
 def doLogin(request):
     username= request.GET.get('username')
@@ -27,16 +38,20 @@ def doLogin(request):
         messages.info(request, 'something wrong during authentication')
         return render(request, 'login.html')
 
+
 def loginPage(request):
     return render(request, 'login.html')
 
 def product(request):
-    product_list = Product.objects.all()
-    number_of_products = product_list.count()
-    return render(request, 'product.html', {
-        'products': product_list,
-        'products_number' : number_of_products,
-    })
+    if is_user_authenticated(request):
+        product_list = Product.objects.all()
+        number_of_products = product_list.count()
+        return render(request, 'product.html', {
+            'products': product_list,
+            'products_number' : number_of_products,
+        })
+    return loginPage(request)
+
 
 
 def updateProductPrice(request):
@@ -47,22 +62,31 @@ def updateProductPrice(request):
         messages.error(request, 'UpdateProduct was not POST')
         return product
 
+
 def card(request):
-    cards = LoyalCard.objects.all()
-    return render(request, 'loyalty_card.html', {'cards': cards})
+    if is_user_authenticated(request):
+        cards = LoyalCard.objects.all()
+        return render(request, 'loyalty_card.html', {'cards': cards})
+    return loginPage(request)
+
 
 def offer(request):
-    if request.POST:
-        new_offer = Offer()
-        new_offer.name = request.POST.get('name')
-        new_offer.description = request.POST.get('description')
-        new_offer.save()
-    offer_list = Offer.objects.all()
-    return render(request, 'offers.html', {'offers': offer_list})
+    if is_user_authenticated(request):
+        if request.POST:
+            new_offer = Offer()
+            new_offer.name = request.POST.get('name')
+            new_offer.description = request.POST.get('description')
+            new_offer.save()
+        offer_list = Offer.objects.all()
+        return render(request, 'offers.html', {'offers': offer_list})
+    return loginPage(request)
 
 
 def finance(request):
-    return render(request, 'finance.html')
+    if is_user_authenticated(request):
+        return render(request, 'finance.html')
+    return loginPage(request)
+
 
 def log_user_out(request):
     logout(request)
